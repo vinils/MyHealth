851570b8bdb3ce679f0c8fdf3051f856c3bda1f3
vls <vini_ls@hotmail.com>
2019-08-27 00:17:44 -0300

    adding groups id

---
+++ weight/groups.sql
@@ -0,0 +1,8 @@
+USE [DataContext]
+
+insert into [Group] (Id, Name, Initials, ParentId, MeasureUnit) VALUES (
+'BFDF8F5E-C514-416D-B6B0-35B196F5CA96', 'Peso', NULL, '47B974F0-00CC-415B-AF2A-93F010491212', NULL)
+
+--INSERT INTO [DataContext]..[Data]
+--SELECT * FROM [DataContextContext-20180321055800]..[Data] WHERE GroupId IN (
+--'BFDF8F5E-C514-416D-B6B0-35B196F5CA96')
