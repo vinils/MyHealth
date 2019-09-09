e83356e2b2c1d25c97e22c8813a29a8aa57dc566
vls <vini_ls@hotmail.com>
2019-09-09 01:09:11 -0300

    weight datas

--- weight/groups.sql
+++ weight/groups.sql
@@ -3,6 +3,11 @@ USE [DataContext]
 insert into [Group] (Id, Name, Initials, ParentId, MeasureUnit) VALUES (
 'BFDF8F5E-C514-416D-B6B0-35B196F5CA96', 'Peso', NULL, '47B974F0-00CC-415B-AF2A-93F010491212', NULL)
 
---INSERT INTO [DataContext]..[Data]
---SELECT * FROM [DataContextContext-20180321055800]..[Data] WHERE GroupId IN (
---'BFDF8F5E-C514-416D-B6B0-35B196F5CA96')
+--insert into [Data]
+--select d1.* from [DataContextContext-20180321055800_2]..[Data] d1
+--left join [DataContext]..[Data] d2
+--	on d2.CollectionDate = d1.CollectionDate and d2.GroupId = d1.GroupId
+--where d1.GroupId in (
+--'BFDF8F5E-C514-416D-B6B0-35B196F5CA96'
+--)
+--and d2.GroupId is null
