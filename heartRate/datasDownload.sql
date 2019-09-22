ad483ce176d18554db9f1f8b5057fd9b04666958
vls <vini_ls@hotmail.com>
2019-09-21 21:41:21 -0300

    adding suport for large files

--- heartRate/datasDownload.sql
+++ heartRate/datasDownload.sql
@@ -1,14 +1,3 @@
-USE [DataContext]
-
-select 'INSERT INTO [DATA] (GroupId, CollectionDate, DecimalValue, StringValue, Discriminator) VALUES (''' +
-Convert(nvarchar(100), GroupId) + ''', ''' + 
-format(CollectionDate,'yyyy-MM-dd HH:mm:ss') + ''', ' + 
-isnull('''' + Convert(nvarchar(100), DecimalValue) + '''', 'NULL') + ', '
-+ ISNULL('''' + StringValue + '''', 'NULL') + ', '''
-+ Discriminator + ''')'
-from [Data]
-where GroupId in (
-'3A6EFE92-00A4-4B1B-BC40-C5B2C83CF0C1',
-'C0EFE267-E8ED-4B79-A125-DB15ABC0780D'
-)
-ORDER BY GroupId, CollectionDate
+version https://git-lfs.github.com/spec/v1
+oid sha256:34eb1dab852b69f9a3fdc891a0bfb0746dbf906f481d1d15019f63889634925d
+size 539
