0b1daac780e4c44aa0e957371e528b9dff5260e4
vls <vini_ls@hotmail.com>
2019-09-21 21:41:21 -0300

    adding suport for large files

--- heartRate/groups.sql
+++ heartRate/groups.sql
@@ -1,16 +1,3 @@
-USE [DataContext]
-
-insert into [Group] (Id, Name, Initials, ParentId, MeasureUnit) VALUES (
-'3A6EFE92-00A4-4B1B-BC40-C5B2C83CF0C1', 'Cardio', NULL, '47B974F0-00CC-415B-AF2A-93F010491212', NULL)
-insert into [Group] (Id, Name, Initials, ParentId, MeasureUnit) VALUES (
-'C0EFE267-E8ED-4B79-A125-DB15ABC0780D', 'Frequência', NULL, '3A6EFE92-00A4-4B1B-BC40-C5B2C83CF0C1', 'BPM')
-
---insert into [Data]
---select d1.* from [DataContextContext-20180321055800_2]..[Data] d1
---left join [DataContext]..[Data] d2
---	on d2.CollectionDate = d1.CollectionDate and d2.GroupId = d1.GroupId
---where d1.GroupId in (
---'3A6EFE92-00A4-4B1B-BC40-C5B2C83CF0C1',
---'C0EFE267-E8ED-4B79-A125-DB15ABC0780D'
---)
---and d2.GroupId is null
+version https://git-lfs.github.com/spec/v1
+oid sha256:c56ae44d51dd79f4f7031d7eee013c81e8e5aac638f4fbb4f7196ad78846cdbc
+size 725
