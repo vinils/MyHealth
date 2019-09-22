52af747231aedc7a3d0bafffe639da22a9279db2
vls <vini_ls@hotmail.com>
2019-09-21 22:43:16 -0300

    improving

--- heartRate/datasDownload.sql
+++ heartRate/datasDownload.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:34eb1dab852b69f9a3fdc891a0bfb0746dbf906f481d1d15019f63889634925d
-size 539
+oid sha256:5a17fe3bb66f5c627bfa9dc8c103a9f025f2c7e7ef561d758121026961c8553d
+size 525
