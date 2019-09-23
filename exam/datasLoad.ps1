8ccc7e06e462c2c2b86158de6f70286f2e07b32f
vls <vini_ls@hotmail.com>
2019-09-23 00:10:42 -0300

    improving load and download of limitdecimaldenormalized

--- exam/datasLoad.ps1
+++ exam/datasLoad.ps1
@@ -2,3 +2,7 @@ $jsonData = Get-Content "$PSScriptRoot\datas.json" -Encoding UTF8
 $uri2 = New-Object System.Uri -ArgumentList ("http://$uri/odata/v4/Datas/BulkInsert")
 Invoke-WebRequest -Method POST -Uri $uri2 -ContentType 'application/json; charset=utf8' -Body ([System.Text.Encoding]::UTF8.GetBytes($jsonData))
 
+#$SQL_SERVER="w19docker6,1433"
+#$SQL_USER="sa"
+#$SQL_PASSWORD="P@ssword"
+cmd.exe /c "set SQL_SERVER=$SQL_SERVER && set SQL_USER=$SQL_USER && set SQL_PASSWORD=$SQL_PASSWORD && limitDecimalDenormalizedsLoad.bat"
---
+++ exam/limitDecimalDenormalizedsDownload.bat
@@ -0,0 +1,5 @@
+::set SQL_SERVER=w19docker6,1433
+::set SQL_USER=sa
+::set SQL_PASSWORD=P@ssword
+
+sqlcmd -S %SQL_SERVER% -U %SQL_USER% -P %SQL_PASSWORD% -d DataContext -t 0 -e -i .\limitDecimalDenormalizedsDownload.sql -o .\limitDecimalDenormalizeds.sql
