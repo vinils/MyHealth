02d1cfc6081b4a88534096ac0b668a7c8beab549
vls <vini_ls@hotmail.com>
2019-09-29 18:16:03 -0300

    adding desc at date for better track

--- heartRate/datasDownload.sql
+++ heartRate/datasDownload.sql
@@ -1,3 +1,14 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5a17fe3bb66f5c627bfa9dc8c103a9f025f2c7e7ef561d758121026961c8553d
-size 525
+USE [DataContext]
+
+select 'INSERT INTO [DATA] (GroupId, CollectionDate, DecimalValue, StringValue, Discriminator) VALUES (''' +
+Convert(nvarchar(100), GroupId) + ''', ''' + 
+format(CollectionDate,'yyyy-MM-dd HH:mm:ss') + ''', ' + 
+isnull(Convert(nvarchar(100), DecimalValue), 'NULL') + ', '
++ ISNULL('''' + StringValue + '''', 'NULL') + ', '''
++ Discriminator + ''')'
+from [Data]
+where GroupId in (
+'3A6EFE92-00A4-4B1B-BC40-C5B2C83CF0C1',
+'C0EFE267-E8ED-4B79-A125-DB15ABC0780D'
+)
+ORDER BY GroupId, CollectionDate desc
