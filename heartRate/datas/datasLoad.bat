e57de151287b832162f82d2f903bc85e28b1213e
vls <vini_ls@hotmail.com>
2019-12-30 00:16:53 -0300

    changing load order

--- heartRate/datas/datasLoad.bat
+++ heartRate/datas/datasLoad.bat
@@ -2,6 +2,6 @@
 ::set SQL_USER=sa
 ::set SQL_PASSWORD=P@ssword
 
-for /f %%f in ('dir /b .\datas*.sql') DO (
+for /f %%f in ('dir /b .\datas*.sql /O:-N') DO (
   sqlcmd -S %SQL_SERVER% -U %SQL_USER% -P %SQL_PASSWORD% -d DataContext -t 0 -e -i .\%%f
 )
