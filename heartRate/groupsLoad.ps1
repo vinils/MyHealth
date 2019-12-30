5e49059fbb387489b4968c4ab50a3406e2448ccd
vls <vini_ls@hotmail.com>
2019-12-30 20:55:22 -0300

    DotNetCore Changes

--- heartRate/groupsLoad.ps1
+++ heartRate/groupsLoad.ps1
@@ -1,3 +1,3 @@
 $jsonData = Get-Content "$PSScriptRoot\groups.json" -Encoding UTF8
 $uri2 = New-Object System.Uri -ArgumentList ("http://$uri/odata/groups")
-Invoke-WebRequest -Method POST -Uri $uri2 -ContentType 'application/json; charset=utf8' -Body ([System.Text.Encoding]::UTF8.GetBytes($jsonData))
+Invoke-WebRequest -Method POST -Uri $uri2 -ContentType 'application/json' -Body ([System.Text.Encoding]::UTF8.GetBytes($jsonData))
