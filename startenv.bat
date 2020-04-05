c46f785e4e04c3727c28dd5ccdb3fa846184f73d
Vinícius Lourenço Santos <vini_ls@hotmail.com>
2020-04-05 17:50:42 -0300

    Update startenv.bat

--- startenv.bat
+++ startenv.bat
@@ -14,6 +14,14 @@ docker-machine scp -r CSharp.Data.Service.pfx dckMyHealth1:~/Https
 cd C:\linuxmyhealthservice\UserSecrets\236d2121-a64b-48d6-8141-bfb4292beb23
 docker-machine scp secrets.json dckMyHealth1:~/UserSecrets/236d2121-a64b-48d6-8141-bfb4292beb23
 
+docker-machine ssh dckmyhealth1 sudo mkdir ~/sqlbkp
+docker-machine ssh dckmyhealth1 sudo chown -R docker ~/sqlbkp
+z:
+cd "z:\DADOS\BackUp\SQL Backup"
+docker-machine scp DataContext_latest.bak dckMyHealth1:~/sqlbkp
+
+
+
 SET DOCKER_TLS_VERIFY=1
 SET DOCKER_HOST=tcp://192.168.15.147:2376
 SET DOCKER_CERT_PATH=C:\Users\MyUser\Desktop\dckMyHealth1
